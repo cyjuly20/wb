e5cd826b9999a381c5e09773481e5b5f6fe324ff
cyjuly20 <cyjuly20@163.com>
2020-05-28 16:15:39 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -109,7 +109,7 @@ class UsersController extends Controller
       });
     }
 
-   public function confirmEmail($token)
+    public function confirmEmail($token)
     {
         $user = User::where('activation_token', $token)->firstOrFail();
 
@@ -121,5 +121,19 @@ class UsersController extends Controller
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
 
+    }
+
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
     }
 }
